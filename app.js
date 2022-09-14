16fce084be5c46b6ac2986e9cf5063d9e76b9fdb
oparaugofavour <oparaugofavour35@gmail.com>
2022-09-14 08:35:28 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('app.js');
+var bitCoin = document.getElementById('bitcoin');
+var ethereum = document.getElementById('ethereum');
+var liteCoin = document.getElementById('liteCoin');
+
+button.onclick = function(){
+    if(bitCoin.className = 'bitcoin'){
+
+    }
+}
+
